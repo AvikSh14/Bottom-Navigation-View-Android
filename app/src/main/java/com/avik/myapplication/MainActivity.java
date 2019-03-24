7f44251c494c8e415b5794efdd3d1c5a9d620c3a
Avik Sharma Chowdhury <avik@surecash.net>
2019-03-24 11:38:31 +0600

    -basic bottom nav view implemented

--- app/src/main/java/com/avik/myapplication/MainActivity.java
+++ app/src/main/java/com/avik/myapplication/MainActivity.java
@@ -1,13 +1,42 @@
 package com.avik.myapplication;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.itemMusic:
+                                Toast.makeText(context, "Clicked music", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.itemGallery:
+                                Toast.makeText(context, "Clicked gallery", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.itemShoppingCart:
+                                Toast.makeText(context, "Clicked cart", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return true;
+                    }
+                }
+        );
     }
 }
